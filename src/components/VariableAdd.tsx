676d2dd9252b4a4a11e95666e92554d55eca01b1
Volodymyr Horbenko <volodymyr4152@gmail.com>
2024-05-01 19:19:00 -0400

    variable add field UI tuning

--- src/components/VariableAdd.tsx
+++ src/components/VariableAdd.tsx
@@ -17,26 +17,24 @@ const VariableAdd = () => {
   };
 
   return (
-    <div className="flex items-center border-b border-gray-200 py-2 mr-5">
-      <div className="flex-1 flex space-x-1 items-center">
+    <div className="flex items-center border-b border-gray-200 py-1 mr-4">
+      <div className="flex-1 flex space-x-1 items-center font-mono">
         <span className="pl-1">name</span>
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="border border-gray-300 rounded px-2 py-0.5 w-20"
+          className="rounded border border-b-2 border-b-blue-700 px-2 py-0.5 w-1/3 font-mono"
         />
         <span className="pl-1">value</span>
         <input
           value={value}
           onChange={(e) => setValue(e.target.value)}
-          className="border border-gray-300 rounded px-2 py-0.5 w-28"
+          className="rounded border border-b-2 border-b-blue-700 px-2 py-0.5 w-2/3 font-mono"
         />
       </div>
-      <div>
-        <button onClick={handleAdd} className="text-gray-600 hover:text-gray-800 hover:bg-amber-200 rounded p-1">
-          <RiAddCircleLine />
-        </button>
-      </div>
+      <button onClick={handleAdd} className="text-gray-600 hover:text-gray-800 hover:bg-amber-200 rounded p-1 ml-2">
+        <RiAddCircleLine />
+      </button>
     </div>
   );
 };
